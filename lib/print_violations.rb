e991502584f613de5019aed291b06c075f839090
amj133 <amj@vt.edu>
2018-02-20 10:03:07 -0700

    end of challenge

--- lib/print_violations.rb
+++ lib/print_violations.rb
@@ -10,7 +10,7 @@ class PrintViolations
 
   def print_contents_to_array(file_path)
     content = read_file(file_path)
-    content.map do |row|
+    @violations_array = content.map do |row|
       Violation.new(row[:violation_id],
                     row[:inspection_id],
                     row[:violation_category],
@@ -18,6 +18,26 @@ class PrintViolations
                     row[:violation_date_closed],
                     row[:violation_type])
     end
+  end
+
+  def count_by_violation_type
+    count_by_violations = Hash.new(0)
+    @violations_array.each do |violation|
+      count_by_violations[violation.violation_type] += 1
+    end
+    count_by_violations
+  end
+
+  def date_of_earliest_violation
+    @violations_array.sort_by do |violation|
+      violation.violation_date
+    end.first.violation_date
+  end
+
+  def date_of_latest_violation
+    @violations_array.sort_by do |violation|
+      violation.violation_date
+    end.last.violation_date
   end
 
 end
--- test/print_violations_test.rb
+++ test/print_violations_test.rb
@@ -13,6 +13,7 @@ class PrintViolationsTest < Minitest::Test
 
   def test_read_file_returns_CSV_class
     table = PrintViolations.new
+
     content = table.read_file('data/fixtures/violations_fixture.csv')
 
     assert_equal CSV, content.class
@@ -20,10 +21,49 @@ class PrintViolationsTest < Minitest::Test
 
   def test_print_contents_to_array_returns_array_of_contents
     table = PrintViolations.new
+
     content = table.print_contents_to_array('data/fixtures/violations_fixture.csv')
 
     assert_equal 4, content.count
     assert_equal "204851", content.first.violation_id
+  end
+
+  def test_count_by_violation_type_returns_hash
+    table = PrintViolations.new
+
+    table.print_contents_to_array('data/fixtures/violations_fixture.csv')
+    count_by_violations = table.count_by_violation_type
+
+    assert_equal({"Refuse Accumulation" => 3,
+                  "Unsanitary conditions, not specified" => 1}, count_by_violations)
+  end
+
+  def test_date_of_earliest_violation
+    table = PrintViolations.new
+
+    table.print_contents_to_array('data/fixtures/violations_fixture.csv')
+    earliest_violation_date = table.date_of_earliest_violation
+
+    assert_equal "2011-01-03 00:00:00", earliest_violation_date
+  end
+
+  def test_date_of_latest_violation
+    table = PrintViolations.new
+
+    table.print_contents_to_array('data/fixtures/violations_fixture.csv')
+    latest_violation_date = table.date_of_latest_violation
+
+    assert_equal "2013-01-03 00:00:00", latest_violation_date
+  end
+
+  def test_print_table
+    skip
+    table = PrintViolations.new
+
+    table.print_contents_to_array('data/fixtures/violations_fixture.csv')
+
+    assert_equal ["Refuse Accumulation, Count: 3, Date of Earliest Violation: 2011-01-03, Date of Latest Violation: 2013-01-03\nUnsanitary conditions, not specified, Count: 1, Date of Earliest Violation: 2013-01-03, Date of Latest Violation: 2013-01-03"],
+      table.print_table
   end
 
 end
